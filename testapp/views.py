b32195173c2f4a436b5d765bdcb4602e60b2e921
mincastle98 <alstjd98522@naver.com>
2021-11-09 17:03:13 +0900

    Add testapp

--- testapp/views.py
+++ testapp/views.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from testapp import views
+from django.http import HttpResponse
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url('', views.index),
-]
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
