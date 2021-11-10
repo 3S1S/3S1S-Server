06795a75e4fade2214b78017d9116b526db6cbd8
KIM MINSEONG <alstjd98522@naver.com>
2021-11-11 00:35:36 +0900

    Update views.py

--- main/views.py
+++ main/views.py
@@ -32,21 +32,21 @@ class SignUp(View):
                     return JsonResponse({'message' : '필수 항목을 모두 입력하세요.'}, status =210)
             
             if not data['is_valid']:
-                return JsonResponse({'message' : 'ID 중복 확인을 수행해주세요.'}, status =211)
+                return JsonResponse({'message' : 'ID 중복 확인을 수행해주세요.'}, status =210)
 
             # 비밀번호 재입력 불일치
             if data['password'] != data['password_check']:
-                return JsonResponse({'message' : '비밀번호가 일치하지 않습니다.'}, status = 212)
+                return JsonResponse({'message' : '비밀번호가 일치하지 않습니다.'}, status = 210)
 
             # 이메일 중복
             if User.objects.filter(email = data['email']).exists():
-                return JsonResponse({'message' : '동일한 이메일로 가입한 회원이 존재합니다.'}, status = 213)
+                return JsonResponse({'message' : '동일한 이메일로 가입한 회원이 존재합니다.'}, status = 210)
             
             else: # 이메일 형식 오류
                 regex= re.compile(r"[a-zA-Z0-9_]+@[a-z]+[.]com")
                 mo = regex.search(data['email'])
                 if mo == None:
-                    return JsonResponse({'message' : '이메일 형식이 옳지 않습니다.'}, status = 214)
+                    return JsonResponse({'message' : '이메일 형식이 옳지 않습니다.'}, status = 210)
             
             User.objects.create(
                 id = data['id'],
@@ -79,9 +79,9 @@ class SignIn(View):
                     token = user.id
                     return JsonResponse({'token' : token}, status=200)
 
-                return JsonResponse({"message" : "Wrong Password"}, status = 211)
+                return JsonResponse({"message" : "Wrong Password"}, status = 210)
               
-            return JsonResponse({"message": "Unexist ID"}, status = 212)
+            return JsonResponse({"message": "Unexist ID"}, status = 210)
         
         except json.JSONDecodeError as e :
             return JsonResponse({'message': f'Json_ERROR:{e}'}, status = 500)
